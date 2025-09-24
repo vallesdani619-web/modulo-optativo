1121a424a2cc0076bcaf0a36c6f8c8d1643cf836
vallesdani619-web <vallesdani619@gmail.com>
2025-09-24 16:25:20 +0200

    Update ejercicio6.py

--- ejercicio6.py
+++ ejercicio6.py
@@ -1,8 +1,9 @@
-print("introduce tu edade")
+print("introduce tu edad")
 edad=int(input())
-if edad < o or edad > 120:
+if edad < 0 or edad > 120:
     print("edad incorrecta")
 elif edad < 18:
     print("el alumno es menor de edad ")
 else:
+
     print("el alumno es mayor de edad")
