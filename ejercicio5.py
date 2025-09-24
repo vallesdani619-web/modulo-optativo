84df9441380e8b4c4518df3c6664ddb378e7370d
vallesdani619-web <vallesdani619@gmail.com>
2025-09-24 16:21:31 +0200

    Add files via upload

---
+++ ejercicio5.py
@@ -0,0 +1,6 @@
+print("introduce un numero que sea par")
+numero=int(input())
+if numero % 2 == 0:
+   print("es par")
+else:
+   print("te he dicho que sea par!!!")
---
+++ ejercicio6.py
@@ -0,0 +1,8 @@
+print("introduce tu edade")
+edad=int(input())
+if edad < o or edad > 120:
+    print("edad incorrecta")
+elif edad < 18:
+    print("el alumno es menor de edad ")
+else:
+    print("el alumno es mayor de edad")
